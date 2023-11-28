dff63d7076d55aea9b45de8ad34e5f62ed0bf67b
mark ramy <markramyy@gmail.com>
2023-11-28 15:44:51 +0200

    Adding me url

--- tenants/urls.py
+++ tenants/urls.py
@@ -11,4 +11,5 @@ app_name = 'tenants'
 urlpatterns = [
     path('create/', views.CreateTenantView.as_view(), name='create'),
     path('token/', views.CreateTokenView.as_view(), name='token'),
+    path('me/', views.ManageTenantView.as_view(), name='me'),
 ]

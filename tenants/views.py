6c590b1c05241665568c5247a42c37fbb403dc7d
mark ramy <markramyy@gmail.com>
2023-11-28 15:26:58 +0200

    Create a new Token view to handel Token Authentication

--- tenants/views.py
+++ tenants/views.py
@@ -2,10 +2,18 @@
 Views for the tenant API.
 """
 from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-from tenants.serializers import TenantSerializer
+from tenants.serializers import TenantSerializer, AuthTokenSerializer
 
 
 class CreateTenantView(generics.CreateAPIView):
     """Create a new tenant in the system."""
     serializer_class = TenantSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for the tenant."""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
